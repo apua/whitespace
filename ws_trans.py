b75268e497b8bbcdf7497c6bbd407e198b655cfd
Apua <apua.a.aa@gmail.com>
2013-11-24 18:40:08 +0800

    recreate a tool to translate, clean, and download Whitespace code

---
+++ ws_trans.py
@@ -0,0 +1,33 @@
+'''
+Goal:
+  usage: CMD -f [] -m [] [] []
+  purpose: download file / trans pure / trans lexical tokens
+
+version: 0.1
+'''
+
+def translate_content(content, orig_token, to_token):
+  return content.translate(str.maketrans(orig_token,to_token))
+
+def get_content(path):
+  if path.startswith('http://'):
+    from urllib.request import urlopen
+    return urlopen(path).read().decode('UTF8')
+  else:
+    return open(path).read()
+
+def clean_content(content, lexical_token=' \t\n'):
+  return ''.join(c for c in content if c in lexical_token)
+
+def output_result(content, output_file=None):
+  if output_file:
+    with open(output_file,'w') as f:
+      f.write(content)
+  else:
+    print(content)
+
+if __name__=='__main__':
+  C = get_content("http://compsoc.dur.ac.uk/whitespace/fibonacci.ws")
+  C = clean_content(C)
+  C = translate_content(C,' \t\n', 'STL')
+  output_result(C)
