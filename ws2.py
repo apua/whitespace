753f609e704f31a06a507756d62791bd66bf23c7
Apua <Apua.A.Aa@gmail.com>
2013-11-28 01:08:05 +0800

    take ``PCs.append`` out of Operations

--- ws2.py
+++ ws2.py
@@ -37,34 +37,34 @@ def run(code):
   num = lambda n: eval('+-'[n[0]!='S']+'0b'+n[1:].translate({83:48,84:49}))
   Operations = [
     # stack manipulation
-    (lambda n,c: PCs.append(Stack.append(num(n)) or c+1) ),
-    (lambda n,c: PCs.append(Stack.append(Stack[-1]) or c+1) ),
-    (lambda n,c: PCs.append(Stack.append(Stack[-num(n)]) or c+1) ),
-    (lambda n,c: PCs.append(Stack.insert(-1,Stack.pop()) or  c+1) ),
-    (lambda n,c: PCs.append(Stack.__delitem__(-1) or c+1) ),
-    (lambda n,c: PCs.append(any(Stack.__delitem__(-2) for t in range(n)) or c+1) ),
+    (lambda n,c: Stack.append(num(n)) or c+1 ),
+    (lambda n,c: Stack.append(Stack[-1]) or c+1 ),
+    (lambda n,c: Stack.append(Stack[-num(n)]) or c+1 ),
+    (lambda n,c: Stack.insert(-1,Stack.pop()) or  c+1 ),
+    (lambda n,c: Stack.__delitem__(-1) or c+1 ),
+    (lambda n,c: any(Stack.__delitem__(-2) for t in range(n)) or c+1 ),
     # arithmetic
-    (lambda n,c: PCs.append(Stack.append(Stack.pop(-2)+Stack.pop()) or c+1) ),
-    (lambda n,c: PCs.append(Stack.append(Stack.pop(-2)-Stack.pop()) or c+1) ),
-    (lambda n,c: PCs.append(Stack.append(Stack.pop(-2)*Stack.pop()) or c+1) ),
-    (lambda n,c: PCs.append(Stack.append(Stack.pop(-2)//Stack.pop()) or c+1) ),
-    (lambda n,c: PCs.append(Stack.append(Stack.pop(-2)%Stack.pop()) or c+1) ),
+    (lambda n,c: Stack.append(Stack.pop(-2)+Stack.pop()) or c+1 ),
+    (lambda n,c: Stack.append(Stack.pop(-2)-Stack.pop()) or c+1 ),
+    (lambda n,c: Stack.append(Stack.pop(-2)*Stack.pop()) or c+1 ),
+    (lambda n,c: Stack.append(Stack.pop(-2)//Stack.pop()) or c+1 ),
+    (lambda n,c: Stack.append(Stack.pop(-2)%Stack.pop()) or c+1 ),
     # heap
-    (lambda n,c: PCs.append(Heap.__setitem__(Stack.pop(-2), Stack.pop()) or c+1) ),
-    (lambda n,c: PCs.append(Stack.append(Heap.__getitem__(Stack.pop())) or c+1) ),
+    (lambda n,c: Heap.__setitem__(Stack.pop(-2), Stack.pop()) or c+1 ),
+    (lambda n,c: Stack.append(Heap.__getitem__(Stack.pop())) or c+1 ),
     # IO
-    (lambda n,c: PCs.append(__import__('sys').stdout.write(chr(Stack.pop())) and __import__('sys').stdout.flush() or c+1) ),
-    (lambda n,c: PCs.append(__import__('sys').stdout.write(str(Stack.pop())) and __import__('sys').stdout.flush() or c+1) ),
-    (lambda n,c: PCs.append(Heap.__setitem__(Stack.pop(),ord(__import__('sys').stdin.read(1))) or c+1) ),
-    (lambda n,c: PCs.append(Heap.__setitem__(Stack.pop(),int(input())) or c+1) ),
+    (lambda n,c: __import__('sys').stdout.write(chr(Stack.pop())) and __import__('sys').stdout.flush() or c+1 ),
+    (lambda n,c: __import__('sys').stdout.write(str(Stack.pop())) and __import__('sys').stdout.flush() or c+1 ),
+    (lambda n,c: Heap.__setitem__(Stack.pop(),ord(__import__('sys').stdin.read(1))) or c+1 ),
+    (lambda n,c: Heap.__setitem__(Stack.pop(),int(input())) or c+1 ),
     # flow
     (lambda n,c: 0),
-    (lambda n,c: PCs.append(eval('CPSR.append({c}+1) or Labels["{n}"]'.format(n=n,c=c))) ),
-    (lambda n,c: PCs.append(eval('Labels["{n}"]'.format(n=n,c=c))) ),
-    (lambda n,c: PCs.append(eval('Labels["{n}"] if Stack.pop()==0 else {c}+1 '.format(n=n,c=c))) ),
-    (lambda n,c: PCs.append(eval('Labels["{n}"] if Stack.pop()<0 else {c}+1 '.format(n=n,c=c))) ),
-    (lambda n,c: PCs.append(eval('CPSR.pop()'.format(n=n,c=c))) ),
-    (lambda n,c: PCs.append(eval('-1'.format(n=n,c=c))) ),
+    (lambda n,c: eval('CPSR.append({c}+1) or Labels["{n}"]'.format(n=n,c=c)) ),
+    (lambda n,c: eval('Labels["{n}"]'.format(n=n,c=c)) ),
+    (lambda n,c: eval('Labels["{n}"] if Stack.pop()==0 else {c}+1 '.format(n=n,c=c)) ),
+    (lambda n,c: eval('Labels["{n}"] if Stack.pop()<0 else {c}+1 '.format(n=n,c=c)) ),
+    (lambda n,c: eval('CPSR.pop()'.format(n=n,c=c)) ),
+    (lambda n,c: eval('-1'.format(n=n,c=c)) ),
   ]
   
   any(
@@ -73,7 +73,7 @@ def run(code):
     for m in __import__('re').finditer(patt, code, 64)
   )
 
-  any(c>-1 and Operations[Instructions[c][0]](Instructions[c][1],c) 
+  any(c>-1 and PCs.append( Operations[Instructions[c][0]](Instructions[c][1],c) )
     for c in PCs) #or result
 
 
