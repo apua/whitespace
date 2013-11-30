38ec5782452c696356f3001dcc955092b3128911
Apua <apua.a.aa@gmail.com>
2013-12-01 04:20:32 +0800

    buff -> B ; result -> A

--- ws3.py
+++ ws3.py
@@ -32,10 +32,9 @@ SSSTL TTT TSSS # [K+a+b]
 TLST # n
 LLL
 '''):
-  
 
-    buff = [str(num)]
-    result = []
+    B = [str(num)]
+    A = []
     S = []
     H = {}
     L = {}
@@ -43,13 +42,13 @@ LLL
     P = [0]
     C = []
     
-    O = 'S.append({});S.append(S[-{}]);any(S.pop(-2) and 0 for t in range({}));S.append(S[-1]);S.insert(-1,S.pop());S.pop() and 0;S.append(S.pop(-2)+S.pop());S.append(S.pop(-2)-S.pop());S.append(S.pop(-2)*S.pop());S.append(S.pop(-2)//S.pop());S.append(S.pop(-2)%S.pop());H.__setitem__(S.pop(-2), S.pop());S.append(H.__getitem__(S.pop()));result.append(chr(S.pop()));result.append(str(S.pop()));H.__setitem__(S.pop(),ord(getchar()));H.__setitem__(S.pop(),int(buff.pop(0)));0;C.append(c+1) or {};{};{} if S.pop()==0 else c+1;{} if S.pop()<0 else c+1;C.pop();-1'.split(';')
+    O = 'S.append({});S.append(S[-{}]);any(S.pop(-2) and 0 for t in range({}));S.append(S[-1]);S.insert(-1,S.pop());S.pop() and 0;S.append(S.pop(-2)+S.pop());S.append(S.pop(-2)-S.pop());S.append(S.pop(-2)*S.pop());S.append(S.pop(-2)//S.pop());S.append(S.pop(-2)%S.pop());H.__setitem__(S.pop(-2), S.pop());S.append(H.__getitem__(S.pop()));A.append(chr(S.pop()));A.append(str(S.pop()));H.__setitem__(S.pop(),ord(getchar()));H.__setitem__(S.pop(),int(B.pop(0)));0;C.append(c+1) or {};{};{} if S.pop()==0 else c+1;{} if S.pop()<0 else c+1;C.pop();-1'.split(';')
     
     any(any(I.append((p,v))if p!=17 else L.__setitem__(v,len(I))for p,v in enumerate(m.groups())if v)for m in __import__('re').finditer('(?:S(?:S([ST]{2,})L|TS([ST]{2,})L|TL([ST]{2,})L|(LS)|(LT)|(LL))|TS(?:(SS)|(ST)|(SL)|(TS)|(TT))|TT(?:(S)|(T))|TL(?:(SS)|(ST)|(TS)|(TT))|L(?:SS([ST]+)L|ST([ST]+)L|SL([ST]+)L|TS([ST]+)L|TT([ST]+)L|(TL)|(LL)))',''.join(c for c in CODE if c in'STL'),64)
     )
     
     any(c>-1 and
-        P.append([S,H,result,buff]and
+        P.append([S,H,A,B]and
           eval( O[I[c][0]].format(
             L.get(I[c][1]) if I[c][0]>2 else
               eval('+-'[I[c][1][0]!='S']+'0b'+I[c][1][1:].translate({83:48,84:49}))
@@ -58,7 +57,7 @@ LLL
         )
       for c in P)
     
-    return ''.join(result)
+    return ''.join(A)
 
 
 if __name__=='__main__':
