0c70498c0bc76a4f254f95c66bee28995c82b44c
Apua <apua.a.aa@gmail.com>
2013-12-01 04:12:33 +0800

    merge parsing method

--- ws3.py
+++ ws3.py
@@ -46,10 +46,7 @@ LLL
     
     O = ['S.append({})','S.append(S[-{}])','any(S.pop(-2) and 0 for t in range({}))','S.append(S[-1])','S.insert(-1,S.pop())','S.pop() and 0','S.append(S.pop(-2)+S.pop())','S.append(S.pop(-2)-S.pop())','S.append(S.pop(-2)*S.pop())','S.append(S.pop(-2)//S.pop())','S.append(S.pop(-2)%S.pop())','H.__setitem__(S.pop(-2), S.pop())','S.append(H.__getitem__(S.pop()))','result.append(chr(S.pop()))','result.append(str(S.pop()))','H.__setitem__(S.pop(),ord(getchar()))','H.__setitem__(S.pop(),int(buff.pop(0)))',0,'C.append(c+1) or {}','{}','{} if S.pop()==0 else c+1','{} if S.pop()<0 else c+1','C.pop()','-1',]
     
-    any(
-      any( I.append((p,v)) if p!=17 else L.__setitem__(v, len(I))
-        for p,v in enumerate(m.groups()) if v )
-      for m in __import__('re').finditer('(?:S(?:S([ST]{2,})L|TS([ST]{2,})L|TL([ST]{2,})L|(LS)|(LT)|(LL))|TS(?:(SS)|(ST)|(SL)|(TS)|(TT))|TT(?:(S)|(T))|TL(?:(SS)|(ST)|(TS)|(TT))|L(?:SS([ST]+)L|ST([ST]+)L|SL([ST]+)L|TS([ST]+)L|TT([ST]+)L|(TL)|(LL)))', ''.join(c for c in CODE if c in 'STL'), 64)
+    any(any(I.append((p,v))if p!=17 else L.__setitem__(v,len(I))for p,v in enumerate(m.groups())if v)for m in __import__('re').finditer('(?:S(?:S([ST]{2,})L|TS([ST]{2,})L|TL([ST]{2,})L|(LS)|(LT)|(LL))|TS(?:(SS)|(ST)|(SL)|(TS)|(TT))|TT(?:(S)|(T))|TL(?:(SS)|(ST)|(TS)|(TT))|L(?:SS([ST]+)L|ST([ST]+)L|SL([ST]+)L|TS([ST]+)L|TT([ST]+)L|(TL)|(LL)))',''.join(c for c in CODE if c in'STL'),64)
     )
     
     any(c>-1 and
