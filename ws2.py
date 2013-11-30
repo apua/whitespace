cb944bf637c710654fb6045d912c3b53c0f8f55a
Apua <apua.a.aa@gmail.com>
2013-11-30 20:24:03 +0800

    use meta programming

--- ws2.py
+++ ws2.py
@@ -1,6 +1,6 @@
 def run(code):
 
-  global PCs, CPSR, Stack, Labels, Heap
+  global PCs, CPSR, Stack, Labels, Heap, num, putchar, getchar
 
   Stack = []
   Heap = {}
@@ -40,36 +40,34 @@ def run(code):
   num = lambda n: eval('+-'[n[0]!='S']+'0b'+n[1:].translate({83:48,84:49}))
   Operations = [
     # stack manipulation
-    (lambda n,c: Stack.append(num(n)) or c+1 ),
-    (lambda n,c: Stack.append(Stack[-num(n)]) or c+1 ),
-    (lambda n,c: any(Stack.pop(-2) and 0 for t in range(num(n))) or c+1 ),
-    (lambda n,c: Stack.append(Stack[-1]) or c+1 ),
-    (lambda n,c: Stack.insert(-1,Stack.pop()) or  c+1 ),
-    (lambda n,c: Stack.pop() and 0 or c+1 ),
+    (lambda n,c: eval( 'Stack.append({n}) or {c}+1'.format(n=num(n),c=c) ) ),
+    (lambda n,c: eval( 'Stack.append(Stack[-{n}]) or {c}+1'.format(n=num(n),c=c) ) ),
+    (lambda n,c: eval( 'any(Stack.pop(-2) and 0 for t in range({n})) or {c}+1'.format(n=num(n),c=c) ) ),
+    (lambda n,c: eval( 'Stack.append(Stack[-1]) or {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'Stack.insert(-1,Stack.pop()) or  {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'Stack.pop() and 0 or {c}+1'.format(n=n,c=c) ) ),
     # arithmetic
-    (lambda n,c: Stack.append(Stack.pop(-2)+Stack.pop()) or c+1 ),
-    (lambda n,c: Stack.append(Stack.pop(-2)-Stack.pop()) or c+1 ),
-    (lambda n,c: Stack.append(Stack.pop(-2)*Stack.pop()) or c+1 ),
-    (lambda n,c: Stack.append(Stack.pop(-2)//Stack.pop()) or c+1 ),
-    (lambda n,c: Stack.append(Stack.pop(-2)%Stack.pop()) or c+1 ),
+    (lambda n,c: eval( 'Stack.append(Stack.pop(-2)+Stack.pop()) or {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'Stack.append(Stack.pop(-2)-Stack.pop()) or {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'Stack.append(Stack.pop(-2)*Stack.pop()) or {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'Stack.append(Stack.pop(-2)//Stack.pop()) or {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'Stack.append(Stack.pop(-2)%Stack.pop()) or {c}+1'.format(n=n,c=c) ) ),
     # heap
-    #(lambda n,c: Heap.__setitem__(Stack.pop(-2), Stack.pop()) or c+1 ),
-    #(lambda n,c: Stack.append(Heap.__getitem__(Stack.pop())) or c+1 ),
-    (lambda n,c: eval( 'Heap.__setitem__(Stack.pop(-2), Stack.pop()) or {1}+1'.format(n,c) ) ),
-    (lambda n,c: eval( 'Stack.append(Heap.__getitem__(Stack.pop())) or {1}+1'.format(n,c) ) ),
+    (lambda n,c: eval( 'Heap.__setitem__(Stack.pop(-2), Stack.pop()) or {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'Stack.append(Heap.__getitem__(Stack.pop())) or {c}+1'.format(n=n,c=c) ) ),
     # IO
-    (lambda n,c: putchar(chr(Stack.pop())) or c+1 ),
-    (lambda n,c: putchar(str(Stack.pop())) or c+1 ),
-    (lambda n,c: Heap.__setitem__(Stack.pop(),ord(getchar())) or c+1 ),
-    (lambda n,c: Heap.__setitem__(Stack.pop(),int(input())) or c+1 ),
+    (lambda n,c: eval( 'putchar(chr(Stack.pop())) or {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'putchar(str(Stack.pop())) or {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'Heap.__setitem__(Stack.pop(),ord(getchar())) or {c}+1'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'Heap.__setitem__(Stack.pop(),int(input())) or {c}+1'.format(n=n,c=c) ) ),
     # flow
-    (lambda n,c: 0),
-    (lambda n,c: CPSR.append(c+1) or Labels[n] ),
-    (lambda n,c: Labels[n] ),
-    (lambda n,c: Labels[n] if Stack.pop()==0 else c+1 ),
-    (lambda n,c: Labels[n] if Stack.pop()<0 else c+1 ),
-    (lambda n,c: CPSR.pop() ),
-    (lambda n,c: -1 ),
+    (lambda n,c: eval( '0'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( 'CPSR.append({c}+1) or {n}'.format(n=Labels[n],c=c) ) ),
+    (lambda n,c: eval( '{n}'.format(n=Labels[n],c=c) ) ),
+    (lambda n,c: eval( '{n} if Stack.pop()==0 else {c}+1'.format(n=Labels[n],c=c) ) ),
+    (lambda n,c: eval( '{n} if Stack.pop()<0 else {c}+1'.format(n=Labels[n],c=c) ) ),
+    (lambda n,c: eval( 'CPSR.pop()'.format(n=n,c=c) ) ),
+    (lambda n,c: eval( '-1'.format(n=n,c=c) ) ),
   ]
   
   any(
