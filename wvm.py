52b45c191aad05cae7453dc25739dc517908d182
Apua <Apua.A.Aa@gmail.com>
2014-04-20 02:14:44 +0800

    Now it can parse ws code

--- wvm.py
+++ wvm.py
@@ -2,7 +2,7 @@
 
 '''
 usage:
-    $ python3 wvm.py $FILE [ $LEXICAL_TOKEN ]
+    $ py3 wvm.py $FILE [ $LEXICAL_TOKEN ]
 
 doctest::
 >>> code = """
@@ -28,6 +28,7 @@ doctest::
 
 def run_wvm(whitespace_code, lexical_token='STL'):
     '''compile Whitespace code and run VM'''
+
     import re
     from pprint import pprint as p
 
@@ -35,31 +36,53 @@ def run_wvm(whitespace_code, lexical_token='STL'):
         return ''.join(c for c in code if c in token)#.translate()
 
     W = clean(whitespace_code)
-    R = regex_pattern = r'''
-        # S:=space, T:=tab, L:=Line Feed
-        S (?: S ([ST][ST]+)L            |   # push n
-              TS([ST][ST]+)L            |   # copy n-th item
-              TL([ST][ST]+)L            |   # slide n items
-              LS()                      |   # dup
-              LT()                      |   # swap
-              LL()                      )|  # drop
-        TS(?: SS()|ST()|SL()|TS()|TT()  )|  # + - * // %
-        TT(?: S ()|T ()                 )|  # store retrieve
-        TL(?: SS()|ST()|TS()|TT() )|        # putchar putnum getchar getnum
-        L (?: SS([ST]+)L                |   # set label
-              ST([ST]+)L                |   # call subroutine
-              SL([ST]+)L                |   # jump to label
-              TS([ST]+)L                |   # jump to label if push ==0
-              TT([ST]+)L                |   # jump to label if push < 0
-              TL()                      |   # end subroutine
-              LL()                      )   # end program
-    '''
-    p([match.groups() for match in re.finditer(R,W,re.VERBOSE)])
+
+    # 根據 instruction set 定義 pattern
+    T = instruction_translate= (
+        {'pattern': r'SS([ST][ST]+)L',  'IR': 'Stack.append({})'},
+        {'pattern': r'STS([ST][ST]+)L', 'IR': 'Stack.append(Stack[-{}])'                    },
+        {'pattern': r'STL([ST][ST]+)L', 'IR': 'any(Stack.pop(-2) and 0 for t in range({}))' },
+        {'pattern': r'SLS()',           'IR': 'Stack.append(Stack[-1])'                     },
+        {'pattern': r'SLT()',           'IR': 'Stack.insert(-1,Stack.pop())'                },
+        {'pattern': r'SLL()',           'IR': 'Stack.pop() and 0'                           },
+        {'pattern': r'TSSS()',          'IR': 'Stack.append(Stack.pop(-2)+Stack.pop())'     },
+        {'pattern': r'TSST()',          'IR': 'Stack.append(Stack.pop(-2)-Stack.pop())'     },
+        {'pattern': r'TSSL()',          'IR': 'Stack.append(Stack.pop(-2)*Stack.pop())'     },
+        {'pattern': r'TSTS()',          'IR': 'Stack.append(Stack.pop(-2)//Stack.pop())'    },
+        {'pattern': r'TSTT()',          'IR': 'Stack.append(Stack.pop(-2)%Stack.pop())'     },
+        {'pattern': r'TTS()',           'IR': 'Heap.__setitem__(Stack.pop(-2), Stack.pop())'},
+        {'pattern': r'TTT()',           'IR': 'Stack.append(Heap.__getitem__(Stack.pop()))' },
+        {'pattern': r'TLSS()',          'IR': 'putchar(chr(Stack.pop()))'                   },
+        {'pattern': r'TLST()',          'IR': 'putchar(str(Stack.pop()))'                   },
+        {'pattern': r'TLTS()',          'IR': 'Heap.__setitem__(Stack.pop(),ord(getchar()))'},
+        {'pattern': r'TLTT()',          'IR': 'Heap.__setitem__(Stack.pop(),int(input()))'  },
+        {'pattern': r'LSS([ST]+)L',     'IR': '# set label'                                 },
+        {'pattern': r'LST([ST]+)L',     'IR': 'CPSR.append(c+1) or {}'                      },
+        {'pattern': r'LSL([ST]+)L',     'IR': '{}'                                          },
+        {'pattern': r'LTS([ST]+)L',     'IR': '{} if Stack.pop()==0 else c+1'               },
+        {'pattern': r'LTT([ST]+)L',     'IR': '{} if Stack.pop()<0 else c+1'                },
+        {'pattern': r'LTL()',           'IR': 'CPSR.pop()'                                  },
+        {'pattern': r'LLL()',           'IR': '-1'                                          },
+        )
+
+    # 這段會把轉譯的 IR 建立出來, 並設定 label
+    I = intermediate_representation = []
+    R = pattern = '|'.join(t['pattern'] for t in T)
+    L = temporary_label_mapping = {}
+    for match in re.finditer(R,W,re.VERBOSE):
+        for k, v in enumerate(match.groups()):
+            if v is not None:
+                if T[k]['IR']=='# set label':
+                    L[v] = len(I)
+                else:
+                    I.append((T[k]['IR'], v))
+                break
+    p(L)
+    p(list(enumerate(I)))
+                
     S = stack = []
     H = heap = {}
     P = program_counter = 0
-    T = text = []
-    I = instruction_set = ()
     run = lambda :None
 
 
